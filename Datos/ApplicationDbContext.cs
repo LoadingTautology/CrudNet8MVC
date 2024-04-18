a27e45f3f98a669dc21988e5ce8c817f024be332
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-04-18 19:28:22 +0200

    Add project files.

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CrudNet8MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet8MVC.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        #region Modelos BD
+        //Agregar los modelos aqui, cada modelo corresponde a una tabla en la BD
+        public DbSet<Contacto> ContactoModelo { get; set; }
+
+        #endregion
+
+
+    }
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudNet8MVC.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El telefono fijo es obligatorio")]
+        public string TelefonoFijo { get; set; }
+
+        [Required(ErrorMessage = "El telefono movil es obligatorio")]
+        public string TelefonoMovil { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string Email { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+
+
+    }
+}
